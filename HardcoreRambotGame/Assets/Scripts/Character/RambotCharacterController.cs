65fafc9c79e98d6f69e1f2fe9ed3cdf86acc8c2e
Kuirak <newdrako@gmx.net>
2013-01-26 01:50:47 +0100

    Added Rotate to Rambot

--- HardcoreRambotGame/Assets/Scripts/Character/RambotCharacterController.cs
+++ HardcoreRambotGame/Assets/Scripts/Character/RambotCharacterController.cs
@@ -1,40 +1,65 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(CharacterController))]
 public class RambotCharacterController : MonoBehaviour
 {
 
-    public float ForwardSpeed = 2;
-    public float StrafeSpeed = 2;
+    public float ForwardSpeed = 10;
+    public float StrafeSpeed = 10;
+    public float JumpStrength = 10;
+    
 
-    private bool CanJump = true;
-
-
+    private CharacterController _controller;
 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    _controller = GetComponent<CharacterController>();
+	    Screen.lockCursor = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	   Strafe();
-       Forward();
-        
+        var moveDir = Strafe();
+        moveDir += Forward();
+        Rotate();
+        moveDir += Jump();
+        _controller.SimpleMove(moveDir);
 	}
 
 
-    private void Strafe()
+    private Vector3 Strafe()
     {
         var horizontalValue = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalValue * StrafeSpeed);
+        return transform.right*horizontalValue*StrafeSpeed;
     }
 
-    private void Forward()
+    private Vector3 Forward()
     {
         var verticalValue = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * verticalValue * ForwardSpeed);
+        return transform.forward*verticalValue*ForwardSpeed;
     }
+
+    private void Rotate()
+    {
+        var horizontalMousevalue = Input.GetAxis("Mouse X");
+        transform.Rotate(transform.up,horizontalMousevalue);
+    }
+
+    private Vector3 Jump()
+    {
+        var jump = Input.GetButtonDown("Jump");
+        
+        if (jump && _controller.isGrounded)
+        {
+            Debug.Log("Jump");
+            return transform.up*JumpStrength;
+        }
+        return Vector3.zero;
+    }
+
+   
 }
