24cda7702e2b0bd5789f3d414c2febdb040cad41
Kuirak <newdrako@gmx.net>
2013-01-26 13:00:41 +0100

    Better Jetpack & Body has now  A  trigger Capsule

--- HardcoreRambotGame/Assets/Scripts/Character/Jetpack.cs
+++ HardcoreRambotGame/Assets/Scripts/Character/Jetpack.cs
@@ -3,57 +3,65 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class Jetpack : MonoBehaviour
 {
-    public float Fuel = 100;
-    public float MaxFuel = 100;
+    public float Fuel = 10;
+    public float MaxFuel = 10;
     public float BurnRate = 10;
     public float RefillRate = 8;
-    public float JetpackStrength = 100;
-    public float GravityStrength = 0.1f;
+    public float Offset = 10;
+    public float JetpackStrength = 40;
+    
 
-    public float MaxEmissionRate = 50;
-    public float EmissionAfterBurn = 50;
+    public float MaxEmissionRate = 150;
+    public float EmissionAfterBurn = 150;
   
 
     private CharacterController _controller;
-    private ParticleSystem[] jetpackstreams;
+    private ParticleSystem[] _jetpackstreams;
 	// Use this for initialization
 	void Start ()
 	{
 	    _controller = GetComponent<CharacterController>();
 	    var jetpack = transform.Find("Body").FindChild("Jetpack");
-	    jetpackstreams = jetpack.GetComponentsInChildren<ParticleSystem>();
-        Debug.Log(jetpackstreams[0] +""+ jetpackstreams[1]);
+	    _jetpackstreams = jetpack.GetComponentsInChildren<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
         var jump = Input.GetButton("Jump");
-        if(Fuel+RefillRate*Time.deltaTime<MaxFuel)
+        if (Fuel + RefillRate * Time.deltaTime < MaxFuel && !jump)
         {
-            Fuel += RefillRate*Time.deltaTime;
+            Debug.Log("refill" + Fuel);
+            Fuel += RefillRate * Time.deltaTime;
+
         }
-	    foreach (var jetpackstream in jetpackstreams)
+	
+	    if (!jump)
 	    {
-	        if(jetpackstream.emissionRate>0)
-	        {
-	            jetpackstream.emissionRate -= EmissionAfterBurn*Time.deltaTime;
-	        }
+            foreach (var jetpackstream in _jetpackstreams)
+            {
+                if (jetpackstream.emissionRate > 0)
+                {
+                    jetpackstream.emissionRate -= EmissionAfterBurn * Time.deltaTime;
+                }
+            }
+            return;
 	    }
-	    if (!jump) return;
 	    if(Fuel> BurnRate*Time.deltaTime)
 	    {
 	        Fuel -= BurnRate*Time.deltaTime;
 	        var moveDir = Vector3.up*JetpackStrength;
-            moveDir += Physics.gravity * GravityStrength;
+           
+            moveDir += _controller.velocity.normalized;
 	        moveDir *= Time.deltaTime;
 	        _controller.Move(moveDir);
-	        foreach (var jetpackstream in jetpackstreams)
+       
+	        foreach (var jetpackstream in _jetpackstreams)
 	        {
 	            jetpackstream.emissionRate = MaxEmissionRate;
+	            jetpackstream.startSpeed = Fuel;
 	        }
 	    }
-
-
+        
 	}
 }
