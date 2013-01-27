ac7c3cd668220e01427b0713963f18e768887d40
Adam Burg <abg@fast.ag>
2013-01-27 13:29:28 +0100

    splash screen + Anleitung

---
+++ HardcoreRambotGame/Assets/Scripts/GUI/SplashScreen.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+	
+	const float splashWaitTime = 5.0f;
+	
+	Texture splashImage;
+	
+	// Use this for initialization
+	void Start () {
+		splashImage = (Texture)Resources.Load("splashscreen2");
+	}
+	
+    void OnGUI()
+
+    {
+        // Draw our texture on screen - full screen
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), splashImage, ScaleMode.StretchToFill, false, 0);
+
+        // Call coroutine to hold splash screen up for a given amount of time
+        StartCoroutine (SplashHold());
+    }
+
+    IEnumerator SplashHold()
+    {
+        yield return new WaitForSeconds(splashWaitTime);
+
+        Application.LoadLevel("StartMenu");
+    }
+}
